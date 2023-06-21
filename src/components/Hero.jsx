c66661ab6c4d0ce109ce57971a088aadee051931
Steven Perez <stevenalp18@gmail.com>
2023-06-20 22:34:47 -0400

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,12 @@
 const Hero = () => {
   return (
     <>
-      <div className="border-2 border-white flex">
+      <div className="border-2 border-white flex flex-wrap sm:flex-nowrap p-20">
         <div className="text-white grow border-white border-2">Text Here</div>
+        <div>
         <div className="text-white border-white border-2">
-          <img
-            width={"500"}
-            src="https://www.pngmart.com/files/22/Programmer-PNG-Photos.png"
-          />
+          <img width='600' src="https://www.pngmart.com/files/22/Programmer-PNG-Photos.png"/>
+        </div>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <navbar className="text-white">
-      <ul className="flex py-4 px-4">
-        <div className="w-1/4">Steven Perez</div>
-        <div className="flex gap-8 px-12 md:gap-16 justify-end w-full">
+      <ul className="flex py-6 px-8 text-xl justify-between">
+        <div className="md:w-1/4">Steven Perez</div>
+        <div className="md:hidden flex justify-end"> Hi there </div>
+        <div className="flex gap-8 px-12 md:gap-16 justify-end w-full hidden md:flex">
           <li>
             <NavLink className="" to="/">
               Home
