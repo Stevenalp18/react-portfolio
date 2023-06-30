222e5b6acb0650d63e3d630cd27401c4c85b4eff
Steven Perez <stevenalp18@gmail.com>
2023-06-30 00:22:54 -0400

    fixed iphone pro 12

--- src/features/HomePageNavArrows.jsx
+++ src/features/HomePageNavArrows.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const HomePageNavArrows = () => {
   return (
     <>
-      <div className="flex w-full p-10 absolute bottom-0 ">
+      <div className="flex w-full absolute bottom-10 ">
         <div className="w-full text-center">
           <NavLink to="/about" className={"justify-center"}>
             <i className="fa-solid fa-circle-arrow-down fa-2xl animate__animated animate__flash" />
