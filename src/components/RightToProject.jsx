5c8162a06c36aca3d48f54eff346ea9fd7e7c6e3
Steven Perez <stevenalp18@gmail.com>
2023-07-30 16:31:55 -0400

    added new chnages to responsiveness with arrows to vw for bottom instead of percent, added bouncein animation for mobile link shareicon

--- src/components/RightToProject.jsx
+++ src/components/RightToProject.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const RightToProject = () => {
   return (
-    <div className="absolute top-[49%] bottom-[49%] md:right-10 right-5">
+    <div className="absolute top-[49%] bottom-[49%] md:right-6 right-4">
       <NavLink to="/about">
         <div className="text-center">
           <i className="fa-solid fa-circle-arrow-right fa-2xl animate-pulse" />
--- src/components/SocialSideBar.jsx
+++ src/components/SocialSideBar.jsx
@@ -1,25 +1,52 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const SocialSideBar = () => {
+  const githubIcon = useRef();
+  const linkedInIcon = useRef();
+  const mailIcon = useRef();
+  const shareIcon = useRef();
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggle = () => {
+    githubIcon.current.classList.toggle("hidden");
+    linkedInIcon.current.classList.toggle("hidden");
+    mailIcon.current.classList.toggle("hidden");
+
+    setToggleMenu(!toggleMenu);
+  };
   return (
     <>
       <div className="fixed right-5 bottom-5 lg:bottom-10 flex flex-col gap-2">
-        <button className="hidden sm:block rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black">
+        <button
+          ref={githubIcon}
+          className="hidden sm:block rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black animate__animated animate__bounceInUp"
+        >
           <a href="https://github.com/Stevenalp18" target="_blank">
             <i className="fa-brands fa-lg fa-github w-full py-6 px-4" />
           </a>
         </button>
-        <button className="hidden sm:block rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black">
-          <a href="www.linkedin.com/in/stevenalp18" target="_blank">
+        <button
+          ref={linkedInIcon}
+          className="hidden sm:block rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black animate__animated animate__bounceInUp"
+        >
+          <a href="https://www.linkedin.com/in/stevenalp18" target="_blank">
             <i className="fa-brands fa-lg fa-linkedin py-6 px-4" />
           </a>
         </button>
-        <button className="hidden sm:block rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black">
-          <a href="https://www.google.com" target="_blank">
+        <button
+          ref={mailIcon}
+          className="hidden sm:block rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black animate__animated animate__bounceInUp"
+        >
+          <a href="mailto:stevenalp18@gmail.com" target="_blank">
             <i className="fa-regular fa-lg fa-envelope py-6 px-4" />
           </a>
         </button>
-        <button className="rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black">
+        <button
+          ref={shareIcon}
+          onClick={handleToggle}
+          className="sm:hidden rounded-3xl font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 active:bg-orange-500 hover:text-black"
+        >
           <i className="fa-solid fa-lg fa-share-nodes py-6 px-4" />
         </button>
       </div>
@@ -28,9 +55,3 @@ const SocialSideBar = () => {
 };
 
 export default SocialSideBar;
-
-{
-  /* <button className="fixed bottom-20 left-5 sm:bottom-10 sm:left-10 px-4 py-3 font-['Agdasima'] rounded-3xl text-white animate- bg-blue-500/70 hover:bg-orange-400 hover:text-black">
-        <i className="fa-regular fa-" />
-      </button> */
-}
