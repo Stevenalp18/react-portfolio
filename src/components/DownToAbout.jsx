420f7b8f4f91704a952c9dcb3ea2421d568840ed
Steven Perez <stevenalp18@gmail.com>
2023-07-30 17:20:32 -0400

    small change to mobile bottom arrow

--- src/components/DownToAbout.jsx
+++ src/components/DownToAbout.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const DownToAbout = () => {
   return (
-    <div className="absolute md:left-[48vw] md:right-[48vw] md:bottom-5 bottom-[5vw] left-[48vw] right-[48vw]">
+    <div className="absolute md:bottom-5 mt-[15vw] sm:mt-0 left-[48vw] right-[48vw]">
       <NavLink to="/projects">
         <div className="text-center">
           <i className="fa-solid fa-circle-arrow-down fa-2xl animate-pulse" />
