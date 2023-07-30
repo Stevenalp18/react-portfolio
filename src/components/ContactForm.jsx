a0f252443061449c72dad8f81f33ba212fb45537
Steven Perez <stevenalp18@gmail.com>
2023-07-30 17:10:11 -0400

    added finishing touches to form autoresponse, polished a few other things, may me done for now, until further revision of project or update of projects once i initiate and finish the upcoming projects

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,12 @@
+import { useRef, useState } from "react";
+
 const ContactForm = () => {
+  const inputName = useRef();
+  const [contactName, setContactName] = useState("");
+  function handleInputName() {
+    return setContactName((prevState) => (prevState = inputName.current.value));
+  }
+
   return (
     <div className="md:px-10 px-4 font-['Agdasima'] text-white m-auto ">
       <div className="text-center mt-10 flex flex-col justify-center">
@@ -18,11 +26,14 @@ const ContactForm = () => {
         {/* Name/Email */}
         <div className="mb-2">
           <input
+            // onChange={(e) => setContactName(e.target.value)}
+            onChange={handleInputName}
+            ref={inputName}
             required
             type="text"
             name="name"
             id="name"
-            placeholder="e.g. john doe"
+            placeholder="Enter Name"
             className="p-2 rounded-xl text-md  w-full bg-slate-800 font-mono"
           />
         </div>
@@ -32,7 +43,7 @@ const ContactForm = () => {
             type="email"
             name="email"
             id="email"
-            placeholder="e.g. johndoe@mail.com"
+            placeholder="Enter Email"
             className="p-2 rounded-xl text-md w-full bg-slate-800 font-mono"
           />
         </div>
@@ -40,11 +51,17 @@ const ContactForm = () => {
         <div className="w-full text-center">
           <textarea
             required
-            name="comment"
+            name="message"
             placeholder="Leave your message here..."
             className="p-2 w-full h-36 rounded-xl text-md bg-slate-800 font-mono"
           ></textarea>
         </div>
+        <input
+          type="hidden"
+          name="_autoresponse"
+          value={`Hi ${contactName} Thank you for reaching out! I appreciate the interest, I will respond as soon as I see this message! In the meantime, enjoy the rest of your day! :)`}
+        />
+        <input type="hidden" name="_template" value="table" />
         <div className="w-full">
           <button
             type="submit"
--- src/components/ContactModal.jsx
+++
@@ -1,3 +0,0 @@
-const ContactModal = () => {};
-
-export default ContactModal;
