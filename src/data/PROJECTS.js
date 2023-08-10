da88bf184a2a7a79629a245b02c5d358d3e417a7
Steven Perez <stevenalp18@gmail.com>
2023-08-09 20:26:36 -0400

    added new quote generator project to projects data file so it can render

--- src/data/PROJECTS.js
+++ src/data/PROJECTS.js
@@ -6,7 +6,7 @@ export const PROJECTSDATA = [
     description:
       "I made this project using React, Tailwind, Hooks, canva for design and combined with flex/grid I was able to make it responsive on all mobile devices. I am proud of how my portfolio turned out compared to my other one, it's simple but nice.",
     image: {
-      url: "/assets/portfolioShot.png",
+      url: "/assets/portfolio.png",
       alt: "Screenshot of Portfolio website",
       width: "400",
     },
@@ -20,7 +20,7 @@ export const PROJECTSDATA = [
     description:
       "I made this project using React and Tailwind. I wanted to practice my initial react skills alongside with tailwind. I personalized it into a mini blog for my travel journey. I try to update it every once in a while so I can have it up to date. It is also responsive!",
     image: {
-      url: "/assets/travelShot.png",
+      url: "/assets/travelJourney.png",
       alt: "Screenshot of Travel Journey website",
       width: "400",
     },
@@ -29,41 +29,41 @@ export const PROJECTSDATA = [
   },
   {
     id: 2,
-    featured: true,
+    featured: false,
     title: "About React Card",
     description:
       "This was one of my first react projects ever made, this was made with pure react and css. I did not use any cool features with this project, curently a placeholder for a future featured project after my website is finalized.",
     image: {
-      url: "/assets/cardShot.png",
+      url: "/assets/reactCard.png",
       alt: "Screenshot of React business card",
       width: "400",
     },
     github: "https://github.com/Stevenalp18/travel-Journey",
     link: "https://about-me-stevenalp18.netlify.app/",
+  },
+  {
+    id: 3,
+    featured: true,
+    title: "Quote Generator",
+    description:
+      "This is a Quote Generator that I created to display working with simple API's, making calls, working with hooks, state, version control, tailwind. You can add or delete quotes you want to save. There is also conditional rendering!",
+    image: {
+      url: "/assets/quoteGenerator.png",
+      alt: "Screenshot of Quote Generator Project",
+      width: "400",
+    },
+    github: "https://github.com/Stevenalp18/quote-generator",
+    link: "https://quote-generator-stevenalp18.netlify.app/",
   },
   // Delete below
   {
-    id: 3,
-    featured: false,
-    title: "Placeholder data",
-    description:
-      "This was one of my first react projects ever made, this was made with pure react and css. I did not use any cool features with this project, curently a placeholder for a future featured project after my website is finalized.",
-    image: {
-      url: "/assets/cardShot.png",
-      alt: "Screenshot of React business card",
-      width: "400",
-    },
-    github: "https://github.com/Stevenalp18/travel-Journey",
-    link: "https://about-me-stevenalp18.netlify.app/",
-  },
-  {
     id: 4,
     featured: false,
-    title: "Placeholder data",
+    title: "PlaceHolder Data, Work In Progress Projects",
     description:
       "This was one of my first react projects ever made, this was made with pure react and css. I did not use any cool features with this project, curently a placeholder for a future featured project after my website is finalized.",
     image: {
-      url: "/assets/cardShot.png",
+      url: "/assets/reactCard.png",
       alt: "Screenshot of React business card",
       width: "400",
     },
@@ -73,11 +73,11 @@ export const PROJECTSDATA = [
   {
     id: 5,
     featured: false,
-    title: "Placeholder data",
+    title: "PlaceHolder Data, Work In Progress Projects",
     description:
       "This was one of my first react projects ever made, this was made with pure react and css. I did not use any cool features with this project, curently a placeholder for a future featured project after my website is finalized.",
     image: {
-      url: "/assets/cardShot.png",
+      url: "/assets/reactCard.png",
       alt: "Screenshot of React business card",
       width: "400",
     },
@@ -87,11 +87,11 @@ export const PROJECTSDATA = [
   {
     id: 6,
     featured: false,
-    title: "Placeholder data",
+    title: "PlaceHolder Data, Work In Progress Projects",
     description:
       "This was one of my first react projects ever made, this was made with pure react and css. I did not use any cool features with this project, curently a placeholder for a future featured project after my website is finalized.",
     image: {
-      url: "/assets/cardShot.png",
+      url: "/assets/reactCard.png",
       alt: "Screenshot of React business card",
       width: "400",
     },
